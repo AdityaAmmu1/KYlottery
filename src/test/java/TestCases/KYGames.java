da825ab0d9aab71d24f233d97aabcdd265192d09
Karpagakamatchi <karpagakamatchi.sethuraman@igt.com>
2022-10-27 13:55:48 +0530

    image name changes

--- src/test/java/TestCases/KYGames.java
+++ src/test/java/TestCases/KYGames.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 //import org.sikuli.script.FindFailed;
 //import org.sikuli.script.Screen;
+import org.sikuli.script.FindFailed;
+import org.sikuli.script.Screen;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -15,7 +17,7 @@ import java.net.URISyntaxException;
 import java.net.URL;
 
 public class KYGames extends config {
-  //public static  Screen screen = new Screen();
+  public static Screen screen = new Screen();
   URL resourceFolderURL =this.getClass().getClassLoader().getResource("images");
   public String baseImagePath;
   public WebDriver driver;
@@ -31,16 +33,16 @@ public class KYGames extends config {
   }
 
   @Test
-  public void a50xthecash() throws URISyntaxException {
+  public void a50xthecash() throws URISyntaxException, FindFailed {
     KYlotteryPage kYlotteryPage = new KYlotteryPage(driver);
 baseImagePath =resourceFolderURL.toURI().getPath() +kYlotteryPage.a50xthecash;
 driver.get(kYlotteryPage.BaseURL_50xthecash);
-//screen.wait(baseImagePath+kYlotteryPage.a50xthecash_info,60).click();
-//screen.wait(baseImagePath+kYlotteryPage.a50xthecash_audio,60).click();
-//screen.wait(baseImagePath+kYlotteryPage.a50xthecash_close,20).click();
-//screen.wait(baseImagePath+kYlotteryPage.a50xthecash_increase,20).click();
-//screen.wait(baseImagePath+kYlotteryPage.a50xthecash_try,20).click();
-//screen.wait(baseImagePath+kYlotteryPage.a50xthecash_revealall,60).click();
-//screen.wait(baseImagePath+kYlotteryPage.a50xthecash_try2);
+screen.wait(baseImagePath+kYlotteryPage.a50xthecash_info,60).click();
+screen.wait(baseImagePath+kYlotteryPage.a50xthecash_audio,60).click();
+screen.wait(baseImagePath+kYlotteryPage.a50xthecash_close,20).click();
+screen.wait(baseImagePath+kYlotteryPage.a50xthecash_increase,20).click();
+screen.wait(baseImagePath+kYlotteryPage.a50xthecash_try,20).click();
+screen.wait(baseImagePath+kYlotteryPage.a50xthecash_revealall,60).click();
+screen.wait(baseImagePath+kYlotteryPage.a50xthecash_try2);
     }
 }
