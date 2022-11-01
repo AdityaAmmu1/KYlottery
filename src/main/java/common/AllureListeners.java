9dba8573becbfecbe4adb885f1359447ea088e8a
Karpagakamatchi <karpagakamatchi.sethuraman@igt.com>
2022-11-01 11:50:39 +0530

    kylotterypage

---
+++ src/main/java/common/AllureListeners.java
@@ -0,0 +1,76 @@
+package common;
+
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class AllureListeners {
+
+
+    public class allureListeners implements ITestListener {
+
+        private static String getTestMethodName(ITestResult iTestResult) {
+            return iTestResult.getMethod().getConstructorOrMethod().getName();
+        }
+
+        @Attachment
+        public byte [] saveFailureScreenShot (WebDriver driver){
+            return ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+        }
+
+        @Attachment(value = "{0}", type = "text/plain")
+        public static String saveTextLog(String message){
+            return message;
+        }
+
+        @Override
+        public void onStart(ITestContext iTestContext){
+            System.out.println("I am in onStart method " + iTestContext.getName());
+            iTestContext.setAttribute("WebDriver", config.getDriver());
+        }
+
+        @Override
+        public void onFinish(ITestContext iTestContext){
+            System.out.println("I am in onFinish method" + iTestContext.getName());
+        }
+
+        @Override
+        public void onTestStart(ITestResult iTestResult){
+            System.out.println("I am in onTestStart method" + getTestMethodName(iTestResult) + "start");
+        }
+
+        @Override
+        public void onTestSuccess(ITestResult iTestResult){
+            System.out.println("I am in onTestSuccess method" + getTestMethodName(iTestResult) + "success");
+        }
+
+        @Override
+        public void onTestFailure(ITestResult iTestResult){
+            System.out.println("I am onTestFailure method" + getTestMethodName(iTestResult) + "failed");
+            Object testClass = iTestResult.getInstance();
+            WebDriver driver = config.getDriver();
+            //Allure Screenshot and Save TestLog
+            if (driver instanceof WebDriver){
+                System.out.println("Screenshot captured for test case:" + getTestMethodName(iTestResult));
+                saveFailureScreenShot(driver);
+            }
+            saveTextLog(getTestMethodName(iTestResult) + "failed and screenshot ta  ken!");
+            driver.navigate().refresh();
+        }
+
+        @Override
+        public void onTestSkipped(ITestResult iTestResult){
+            System.out.println("I am in onTestSkipped method " + getTestMethodName(iTestResult));
+        }
+
+        @Override
+        public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult){
+            System.out.println("Test failed but it is in defined success ratio " + getTestMethodName(iTestResult));
+        }
+
+    }
+}
--- src/test/java/TestCases/KYGames.java
+++ src/test/java/TestCases/KYGames.java
@@ -3,6 +3,8 @@ package TestCases;
 import PageObject.KYlotteryPage;
 import common.config;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.sikuli.script.FindFailed;
@@ -19,6 +21,7 @@ public class KYGames extends config {
   URL resourceFolderURL =this.getClass().getClassLoader().getResource("images");
   public String baseImagePath;
   public WebDriver driver;
+  public static Logger logger = LogManager.getLogger(KYGames.class);
 
 
   @BeforeClass(description = "Go to Ky Lottery")
@@ -34,16 +37,17 @@ public class KYGames extends config {
   @Test
   public void a50xthecash() throws URISyntaxException, FindFailed
   {
+
     KYlotteryPage kYlotteryPage = new KYlotteryPage(driver);
 baseImagePath =resourceFolderURL.toURI().getPath() +kYlotteryPage.a50xthecash;
 driver.get(kYlotteryPage.BaseURL_50xthecash);
-screen.wait(baseImagePath+kYlotteryPage.a50xthecash_info,60).click();
+screen.wait(baseImagePath+kYlotteryPage.a50xthecash_info,100).click();
 screen.wait(baseImagePath+kYlotteryPage.a50xthecash_audio,60).click();
 screen.wait(baseImagePath+kYlotteryPage.a50xthecash_close,20).click();
 screen.wait(baseImagePath+kYlotteryPage.a50xthecash_increase,20).click();
 screen.wait(baseImagePath+kYlotteryPage.a50xthecash_try,20).click();
 screen.wait(baseImagePath+kYlotteryPage.a50xthecash_revealall,60).click();
-screen.wait(baseImagePath+kYlotteryPage.a50xthecash_try2);
+screen.wait(baseImagePath+kYlotteryPage.a50xthecash_try2,30);
   }
 
     @Test
